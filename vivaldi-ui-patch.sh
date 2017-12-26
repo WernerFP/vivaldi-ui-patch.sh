be4d6d1cc89193d83d41da70f65ff0f957ed9dda
WernerFP <wernerfp@posteo.de>
2017-12-26 03:16:09 +0100

    Aesthetics fixed

--- vivaldi-ui-patch.sh
+++ vivaldi-ui-patch.sh
@@ -10,7 +10,7 @@
 # directory that contains the script.
 # The script offers to create a backup of browser.html.
 
-ITSME=$( basename $(readlink -f $0) )
+ITSME=$( basename $( readlink -f $0) )
 
 if [[ -z $PWD ]]; then
 	echo  "Sorry, \$PWD is not set in $0."
@@ -20,7 +20,7 @@ elif [ $UID != 0 ]; then
 	exit
 fi
 
-V="$( dirname $( find /opt -name "vivaldi-bin" ) 2>/dev/null )"
+V=$( dirname $( find /opt -name "vivaldi-bin" ) 2>/dev/null )
 CC=$PWD/custom.css
 CJ=$PWD/custom.js
 DL="/resources/vivaldi"
@@ -52,7 +52,7 @@ fi
 read -p "► Should we backup browser.html first? [y/n]: " OP;
 if  [[ -z $OP ]] || [[ $OP == [yYjJ] ]]; then
 	BU="$PWD/browser.html-$( date +"%Y%m%d_%H%M" ).bak"
-	cp "$VD/resources/vivaldi/browser.html" "$BU"
+	cp "$VD/$DL/browser.html" "$BU"
 	chown -c $USER "$BU" 2>&1> /dev/null; chmod -f 644 "$BU"; chgrp -f users "$BU"
 fi
 
@@ -60,7 +60,7 @@ QCC=$(grep "style/custom.css" "$VD$DL/browser.html" )
 QCJ=$(grep "custom.js" "$VD$DL/browser.html" )
 if [[ -z $QCC ]]; then
 	sed -i 's/  <\/head>/    –––\n&/g' "$VD$DL/browser.html"
-	DONE=" browser.html patched"
+	DONE=" browser.html is patched"
 fi
 if [[ -z $QCJ ]]; then
 	sed -i 's/  <\/body>/    ––\n&/g' "$VD$DL/browser.html"
@@ -69,11 +69,11 @@ fi
 sed -i 's/–––/<link rel=\"stylesheet\" href=\"style\/custom.css\" \/>/g' "$VD$DL/browser.html"
 sed -i 's/––/<script src=\"custom.js\"><\/script>/g' "$VD$DL/browser.html"
 if [[ -f $CC ]]; then
-	cp -f "$PWD/custom.css" "$VD/resources/vivaldi/style/custom.css" 2>/dev/null
+	cp -f "$PWD/custom.css" "$VD$DL/style/custom.css" 2>/dev/null
 	DONE=$(echo -e "$DONE\n custom.css is updated")
 fi
 if [[ -f $CJ ]]; then
-	cp -f "$PWD/custom.js" "$VD/resources/vivaldi/custom.js" 2>/dev/null
+	cp -f "$PWD/custom.js" "$VD$DL/custom.js" 2>/dev/null
 	DONE=$(echo -e "$DONE\n custom.js is updated")
 fi
 echo -e "-----------------------------------------------------------\nCompleted for $VD:\n$DONE"
